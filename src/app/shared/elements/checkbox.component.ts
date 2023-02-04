c22c3814a1cf459f5259f7dcf422c51062e956a5
LDebove <59819894+LDebove@users.noreply.github.com>
2023-02-04 19:21:17 +0100

    implemented ControlValueAccessor for checkbox, select, toggle components

--- src/app/shared/elements/checkbox.component.ts
+++ src/app/shared/elements/checkbox.component.ts
@@ -1,24 +1,57 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'cCheckbox',
   template: `
     <div class="custom-checkbox-container">
-      <input class="checkbox" type="checkbox" [(ngModel)]="checked" (change)="onChange()">
+      <input class="checkbox" [formControl]="input" type="checkbox" (change)="onChange()">
       <label *ngIf="label !== ''" class="checkbox-label">
         <span>{{ label }}</span>
       </label>
     </div>
   `,
   styleUrls: ['./elements.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true
+    }
+  ]
 })
-export class CheckboxComponent {
+export class CheckboxComponent implements ControlValueAccessor {
   @Input('label') label: string = '';
   @Input('checked') checked: boolean = false;
 
   @Output() checkedChange = new EventEmitter<boolean>();
 
+  //#region ControlValueAccessor
+  input = new FormControl('');
+  _onTouched = () => {};
+  //#endregion
+
   onChange(): void {
     this.checkedChange.emit(this.checked);
   }
+
+  //#region ControlValueAccessor
+  writeValue(obj: any): void {
+    if(obj) {
+      this.input.patchValue(obj);
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.input.valueChanges.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.input.disable() : this.input.enable();
+  }
+  //#endregion
 }
--- src/app/shared/elements/select.component.ts
+++ src/app/shared/elements/select.component.ts
@@ -1,23 +1,36 @@
-import { AfterViewInit, Component, ContentChildren, ElementRef, EventEmitter, Output, QueryList, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ContentChildren, ElementRef, EventEmitter, forwardRef, Output, QueryList, ViewChild } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { OptionComponent } from './option.component';
 
 @Component({
   selector: 'cSelect',
   template: `
     <div class="custom-select">
-      <select #select>
+      <select [formControl]="control" #select>
         <ng-content></ng-content>
       </select>
     </div>
   `,
-  styleUrls: ['./elements.scss']
+  styleUrls: ['./elements.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true
+    }
+  ]
 })
-export class SelectComponent implements AfterViewInit {
+export class SelectComponent implements AfterViewInit, ControlValueAccessor {
   @Output() selectionChange: EventEmitter<any> = new EventEmitter<any>();
 
   @ViewChild('select') select!: ElementRef;
 
   @ContentChildren(OptionComponent) optionComponents!: QueryList<OptionComponent>;
+
+  //#region ControlValueAccessor
+  control = new FormControl('');
+  _onTouched = () => {};
+  //#endregion
 
   ngAfterViewInit(): void {
     this.optionComponents?.toArray().forEach((optionComponent) => {
@@ -28,4 +41,24 @@ export class SelectComponent implements AfterViewInit {
       });
     })
   }
+
+  //#region ControlValueAccessor
+  writeValue(obj: any): void {
+    if(obj) {
+      this.control.patchValue(obj);
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.control.valueChanges.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.control.disable() : this.control.enable();
+  }
+  //#endregion
 }
