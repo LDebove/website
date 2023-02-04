deb6006b23912248c7fc0ced7d738abbd56fa850
LDebove <59819894+LDebove@users.noreply.github.com>
2023-02-04 18:27:41 +0100

    solved compilation error (forgot to remove call to a function)

--- src/app/core/terraria-pixelart/terraria-pixelart.component.ts
+++ src/app/core/terraria-pixelart/terraria-pixelart.component.ts
@@ -1,20 +1,12 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-terraria-pixelart',
   templateUrl: './terraria-pixelart.component.html',
   styleUrls: ['./terraria-pixelart.component.scss']
 })
-export class TerrariaPixelartComponent implements AfterViewInit {
-
-  group = new FormGroup({
-    input: new FormControl('')
-  });
+export class TerrariaPixelartComponent {
 
   constructor() { }
-
-  ngAfterViewInit(): void {
-  }
 
 }
